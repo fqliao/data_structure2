60bd849d826c5c8a75a9a4868ed68ee06629f777
fqliao <fqliao@yeah.net>
2017-02-15 20:59:06 +0800

    调试代码

---
+++ src/recursion/NumberCombine.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NumberCombine {
+
+	public static void main(String[] args) {
+		String[] array = new String[]{"1","2","3"};
+		listAll(Arrays.asList(array), "");
+	}
+	
+	public static void listAll(List candidate, String prefix){
+		if(!prefix.equals("")){
+			System.out.println(prefix);
+		}
+		for(int i = 0; i < candidate.size(); i++){
+			List temp = new LinkedList(candidate);
+			listAll(temp, prefix + temp.remove(i));
+		}
+	}
+
+}
